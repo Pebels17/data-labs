d95e7c101f646640cb75951e49ae51e07edbf5d9
Pablo Matanzo <pablo.matanzo@gmail.com>
2019-05-20 16:39:31 +0200

    lab-mysq

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf600
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 mysql> UPDATE SALESPERSON SET STORE = 'Miami' where STAFF_ID = '00005'; ---UPPDATE MIAMI\
+\
+---UPDATES EMAIL\
+\
+mysql> UPDATE CUSTOMERS SET EMAIL = 'ppicasso@gmail.com' WHERE CUSTOMERS_ID ='10001'; \
+\
+\
+mysql> UPDATE CUSTOMERS SET EMAIL = 'lincoln@us.gov' WHERE CUSTOMERS_ID ='20001';\
+\
+\
+mysql> UPDATE CUSTOMERS SET EMAIL = ' hello@napoleon.me' WHERE CUSTOMERS_ID ='30001';}
